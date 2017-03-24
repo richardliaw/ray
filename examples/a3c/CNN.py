73e5645d1120b834509eae60d3c7a96c7d7044a7
Richard <rliaw@berkeley.edu>
2017-03-24 11:27:50 -0400

    cnn model

--- examples/a3c/CNN.py
+++ examples/a3c/CNN.py
@@ -11,13 +11,12 @@ from policy import *
 
 class CNNPolicy(Policy):
 
-    def setup_graph(self, hparams):
-    	print [None] + (hparams['input_size'])
-        self._input = tf.placeholder(tf.float32,
-                shape=[None] + (hparams['input_size'])) # TODO!
+    def setup_graph(self, ob_space, ac_space):
+        self.x = tf.placeholder(tf.float32,
+                shape=[None] + list(ob_space)) # TODO!
 
         conv1 = tf.contrib.layers.convolution2d(
-                inputs=self._input,
+                inputs=self.x,
                 num_outputs=16,
                 kernel_size=(8, 8),
                 stride=4,
@@ -41,7 +40,7 @@ class CNNPolicy(Policy):
                 num_outputs=hparams['num_actions'],
                 activation_fn=None)
 
-        self._value = tf.contrib.layers.fully_connected(
+        self.vf = tf.contrib.layers.fully_connected(
                 inputs=fc1,
                 num_outputs=1,
                 activation_fn=None,
@@ -49,28 +48,29 @@ class CNNPolicy(Policy):
 
         # op to sample an action - multinomial takes unnormalized log probs
         # self.logits = tf.Print(self.logits, [self.logits], "self.Logits - ")
-        self._sample = tf.reshape(tf.multinomial(self.logits, 1), [])
+        self.sample = tf.reshape(tf.multinomial(self.logits, 1), []) #TODO: change to categorical?
+        self.var_list = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, tf.get_variable_scope().name)
+        self.global_step = tf.get_variable("global_step", [], tf.int32, initializer=tf.constant_initializer(0, dtype=tf.int32),
+                                                   trainable=False)
 
     def get_gradients(self, batch):
-        """Computing the gradient is actually model-dependent.
-            The LSTM needs its hidden states in order to compute the gradient accurately."""
+        """Computing the gradient is actually model-dependent."""
+        # TODO: Fix batch object here
         feed_dict = {
             self.x: batch.si,
             self.ac: batch.a,
             self.adv: batch.adv,
             self.r: batch.r,
-            self.state_in[0]: batch.features[0],
-            self.state_in[1]: batch.features[1],
         }
         self.local_steps += 1
         return self.sess.run(self.grads, feed_dict=feed_dict)
 
     def act(self, ob, c, h):
-        return self.sess.run([self.sample, self.vf] + self.state_out,
-                        {self.x: [ob], self.state_in[0]: c, self.state_in[1]: h})
+        return self.sess.run([self.sample, self.vf],
+                        {self.x: [ob]})
 
     def value(self, ob, c, h):
-        return self.sess.run(self.vf, {self.x: [ob], self.state_in[0]: c, self.state_in[1]: h})[0]
+        return self.sess.run(self.vf, {self.x: [ob]})[0]
 
     def get_initial_features(self):
         return self.state_init
