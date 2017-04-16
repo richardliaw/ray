6285fbddc5ba935c5bf2c12b453f648a990ea8c8
Richard Liaw <rliaw@berkeley.edu>
2017-04-16 02:44:54 +0000

    new res

--- examples/a3c/scripts/run_sat.sh
+++ examples/a3c/scripts/run_sat.sh
@@ -3,9 +3,12 @@ for x in {0..2}
 do
     for i in 1 2 4
     do
-        for j in 3 6
+        for j in 100 30 10  
         do 
-            python multimodel.py --num-experiments $i --runners $j 
+            python multimodel.py --num-experiments $i --adam 0.001 --sync $j
+            python multimodel.py --num-experiments $i --adam 0.0001 --sync $j
+            python multimodel.py --num-experiments $i --adam 0.00001 --sync $j
+            python multimodel.py --num-experiments $i --adam 0.000001 --sync $j
         done
     done
 done
