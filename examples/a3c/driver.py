9dfb3504f7c29a91a2a84b54d85ceef391ad49cf
Richard <rliaw@berkeley.edu>
2017-04-20 20:36:45 +0000

    git what a pickle

--- examples/a3c/driver.py
+++ examples/a3c/driver.py
@@ -1,7 +1,7 @@
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
-
+import pickle
 import ray
 import argparse
 from collections import defaultdict
