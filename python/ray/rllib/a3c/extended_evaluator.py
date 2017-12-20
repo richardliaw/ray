c32477c95055d3d390781eec4a5ce09027b76b65
Richard Liaw <rliaw@berkeley.edu>
2017-12-19 20:15:45 -0800

    sharding with new task scheduling?

--- python/ray/rllib/a3c/extended_evaluator.py
+++ python/ray/rllib/a3c/extended_evaluator.py
@@ -3,6 +3,7 @@ from __future__ import division
 from __future__ import print_function
 
 import ray
+import numpy as np
 from ray.rllib.envs import create_and_wrap
 from ray.rllib.optimizers import Evaluator
 from ray.rllib.a3c.common import get_policy_cls
@@ -14,25 +15,46 @@ from ray.rllib.utils.process_rollout import process_rollout
 
 class ShardA3CEvaluator(A3CEvaluator):
 
-    def _init(self):
-        super(ExtendedA3CEvaluator, self)._init()
-        self.ps_count = self.config["ps_count"]
-
-    def compute_deltas(self, *shards):
-        weight_list = []
-        for s in shards:
-            weight_list += s
-
-        weights = {k: delt for k, delt in weight_list}
-        self.policy.set_weights(weights)
+    def compute_deltas(self, *shards): # NEED object IDs
+        old_weights = reconstruct_weights(shards)
+        self.set_flat(old_weights)
         grad, _ = self.compute_gradients(self.sample())
         self.apply_gradients(grad)
-        new_weights = self.get_weights()
-        return sorted([(k, new_weights[k] - weights[k]) for k in new_weights]) # this is split
+        new_weights = self.get_flat()
+        return shard(new_weights - old_weights, len(shards))
+
+    def get_flat(self):
+        return self.policy.variables.get_flat()
+
+    def set_flat(self, weights):
+        return self.policy.variables.set_flat(weights)
 
 
-def setup_sharded(rets):
+def setup_sharded(num_shards):
     ShardA3CEvaluator.compute_deltas = ray.method(
-        num_return_vals=rets)(ShardA3CEvaluator.compute_deltas)
+        num_return_vals=num_shards)(ShardA3CEvaluator.compute_deltas)
 
     return ray.remote(ShardA3CEvaluator)
+
+def shard(array, num):
+    return np.array_split(array, num)
+
+def reconstruct_weights(shards):
+    return np.concatenate(shards)
+
+# def _split_iterator(k, weight, num):
+#     return ((k, split) for split in np.array_split(weight, num))
+
+# def shard(weight_dict, num):
+#     shard_iter = zip(*(_split_iterator(k, weight, num) for k, weight in weight_dict.items()))
+#     return [dict(shard) for shard in shard_iter]
+
+# def reconstruct_weights(shard_list):
+#     weight_dict = {k: [] for k in shard_list[0]}
+#     for shard in shard_list:
+#         for k, weight in shard.items():
+#             weight_dict[k].append(weight)
+
+#     return {k: np.concatenate(v) for k, v in weight_dict.items()}
+
+
--- python/ray/rllib/optimizers/sharded_optimizer.py
+++ python/ray/rllib/optimizers/sharded_optimizer.py
@@ -1,109 +1,109 @@
 import ray
 import numpy as np
 from ray.rllib.optimizers.optimizer import Optimizer
+from ray.rllib.a3c.extended_evaluator import ShardA3CEvaluator, setup_sharded, shard
 
-
+@ray.remote
 class ParameterServer(object):
-    def __init__(self, weight_shard: list):
-        self.params = {k: v.copy() for k, v in weight_shard}
+    def __init__(self, weight_shard: np.ndarray):
+        # self.params = {k: v.copy() for k, v in weight_shard}
+        self.params = weight_shard.copy()
+        print(self.params.shape)
 
-    def update_and_get_weights(self, *deltas):
-        print([k for k, v in deltas])
-        for k, delt in deltas:
-            self.params[k] += delt
+    def update_and_get_weights(self, deltas):
+        print(sum(deltas), self.params.shape)
+        self.params += deltas
         return self.get_weights()
 
     def get_weights(self):
-        weights = list(self.params.items())
-        return weights
+        # weights = list(self.params.items())
+        # weights =
+        return self.params
 
     def ip(self):
         return ray.services.get_node_ip_address()
 
-def create_ps(weight_shard: list):
-    RemotePS = ray.remote(ParameterServer)
-    return RemotePS.remote(weight_shard)
+# def create_ps(weight_shard: np.ndarray):
+#     RemotePS = ray.remote(ParameterServer)
+#     return RemotePS.remote(weight_shard)
 
 
 class ShardedPS():
-    def __init__(self, weight_dict, ps_count):
+    def __init__(self, weights, ps_count):
         self.ps_dict = {}
-        self._num_weights = len(weight_dict)
-        self.indices = {i: [] for i in range(ps_count)}
 
         # populate indices
-        name_sorted_keys = list(sorted(weight_dict))
-        size_sorted_weights = list(sorted(
-            weight_dict, key=lambda k: -len(weight_dict[k].flatten())))
-        size = {i: 0 for i in range(ps_count)}
-        for k in size_sorted_weights:
-            wsize = len(weight_dict[k].flatten())
-            min_ps_idx = min(size, key=lambda j: size[j])
-            size[min_ps_idx] += wsize
-            self.indices[min_ps_idx].append(name_sorted_keys.index(k))
+        # name_sorted_keys = list(sorted(weight_dict))
+        # size_sorted_weights = list(sorted(
+        #     weight_dict, key=lambda k: -len(weight_dict[k].flatten())))
+        # size = {i: 0 for i in range(ps_count)}
+        # for k in size_sorted_weights:
+        #     wsize = len(weight_dict[k].flatten())
+        #     min_ps_idx = min(size, key=lambda j: size[j])
+        #     size[min_ps_idx] += wsize
+        #     self.indices[min_ps_idx].append(name_sorted_keys.index(k))
 
         # start pss
-        for ps_id in range(ps_count):
-            keys = [name_sorted_keys[i] for i in self.get_indices(ps_id)]
-            self.ps_dict[ps_id] = create_ps(
-                [(k, weight_dict[k].copy()) for k in keys])
+        for ps_id, weight_shard in enumerate(shard(weights, ps_count)):
+            # keys = [name_sorted_keys[i] for i in self.get_indices(ps_id)]
+            self.ps_dict[ps_id] = ParameterServer.remote(weight_shard)
 
-
-    def get_indices(self, ps_id):
-        return self.indices[ps_id]
-
-    def update(self, sorted_deltas: list):
-        assert len(sorted_deltas) == self._num_weights
+    def update(self, sharded_deltas: list):
         weight_ids = []
-        for ps_id, ps in self.ps_dict.items():
-            shard = [sorted_deltas[i] for i in self.get_indices(ps_id)]
-            weight_ids.append(ps.update_and_get_weights.remote(*shard))
+        for ps_id, weight_shard in enumerate(sharded_deltas):
+            weight_ids.append(
+                self.ps_dict[ps_id].update_and_get_weights.remote(weight_shard))
         return weight_ids
 
     def get_weight_ids(self):
-        return [ps.get_weights.remote()
-                    for ps in self.ps_dict.values()]
+        return [self.ps_dict[ps_id].get_weights.remote() for ps_id in sorted(self.ps_dict)]
 
 
 class PSOptimizer(Optimizer):
     def _init(self):
-        weights = self.local_evaluator.policy.get_weights()
+        weights = self.local_evaluator.get_flat()
         self.ps = ShardedPS(weights, self.config["ps_count"])
 
     def step(self):
         # send deltas to parameter servers
         # import ipdb; ipdb.set_trace()
         weight_ids = self.ps.get_weight_ids()
-
-        tasks = []
+        worker_to_shard = {}
+        shard_to_worker = {}
         for w in self.remote_evaluators:
-            tasks.append((w, self.update_ps(w, weight_ids)))
+            shards = w.compute_deltas.remote(*weight_ids)
+            worker_to_shard[w] = shards
+            shard_to_worker.update({t: w for t in shards})
 
         for i in range(10):
-            w, next_weight_list = tasks.pop(0)
-            ray.wait(next_weight_list) # is this needed?
-            tasks.append((w, self.update_ps(w, next_weight_list)))
+            [done_shard], _ = ray.wait(list(shard_to_worker))
+            w = shard_to_worker[done_shard]
 
-    def update_ps(self, worker, cur_weights):
-        delta_ids = worker.compute_deltas.remote(*cur_weights)
-        weight_ids = self.ps.update(delta_ids)
-        return weight_ids
+            all_w_shards = worker_to_shard.pop(w)
+            [shard_to_worker.pop(s) for s in all_w_shards]
+            weight_ids = self.ps.update(all_w_shards)
+
+            shards = w.compute_deltas.remote(*weight_ids)
+            worker_to_shard[w] = shards
+            shard_to_worker.update({t: w for t in shards})
+            print(shard_to_worker)
 
 
 if __name__ == '__main__':
-    from ray.rllib.a3c.extended_evaluator import ShardA3CEvaluator, setup_sharded
     from ray.rllib.a3c import DEFAULT_CONFIG
     import gym
     ray.init()
     env_creator = lambda: gym.make("CartPole-v0")
     config = DEFAULT_CONFIG.copy()
     config["use_lstm"] = False
-    config["ps_count"] = 2
-    config["num_workers"] = 1
+    config["ps_count"] = 6
+    config["num_workers"] = 2
     logdir = "/tmp/shard"
 
     local_evaluator = ShardA3CEvaluator(env_creator, config, logdir)
-    RemoteEAEvaluator = setup_sharded(len(local_evaluator.policy.get_weights()))
+    # RemoteEAEvaluator = setup_sharded(len(local_evaluator.policy.get_weights()))
+    RemoteEAEvaluator = setup_sharded(config["ps_count"])
+
     remotes = [RemoteEAEvaluator.remote(env_creator, config, logdir)
                     for i in range(config["num_workers"])]
     optimizer = PSOptimizer(config, local_evaluator, remotes)
