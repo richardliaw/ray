2d260d1ef0cf7a893280e5a0baf80b3ab2c16200
Richard Liaw <rliaw@berkeley.edu>
2019-04-30 14:38:09 -0700

    less

--- python/ray/tune/examples/tune_mnist_keras.py
+++ python/ray/tune/examples/tune_mnist_keras.py
@@ -28,7 +28,7 @@ def train_mnist(config, reporter):
     model.add(Conv2D(
         32, kernel_size=(3, 3), activation="relu", input_shape=input_shape))
     model.add(Conv2D(64, (3, 3), activation="relu"))
-    model.add(MaxPooling2D(pool_size=(config["poolsize"], config["poolsize"])))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Dropout(0.5))
     model.add(Flatten())
     model.add(Dense(config["hidden"], activation="relu"))
